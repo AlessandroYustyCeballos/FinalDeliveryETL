76e80991c9bd6212fcdf051feb20b5b70d8e7987
Alessandro Yusty <alesscreeper@gmail.com>
2026-05-25 12:00:48 -0500

    Delete exp directory

--- exp/test.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import pandas as pd
-conn = sqlite3.connect('DB.db')
-cursor = conn.cursor()
-#print(cursor.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall())
-data = cursor.execute("SELECT * FROM stream").fetchall()
-df = pd.DataFrame(data)
-print(df)
-conn.close()
